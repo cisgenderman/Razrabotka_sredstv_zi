641b5e054826d00bf9ae08ef1ab6505e4f4ae2d4
cisgenderman <grigorjevsergey1@gmail.com>
2026-02-26 18:17:22 +0700

    Продолжаем писать

--- cryptomanager.cpp
+++ cryptomanager.cpp
@@ -16,4 +16,40 @@ cryptomanager* cryptomanager::getInstance();
     return instance;
 }
 
-QByteArray cryptomanager::encrypt(const )
+QByteArray cryptomanager::encrypt(const QByteArray &data, const QByteArray &key)
+{
+    if (key.isEmpty())
+    {
+        return data;
+    }
+
+    QByteArray result = data;
+    int keySize = key.size();
+
+    for (int i = 0; i<result.size(); i++)
+    {
+        result[i] = result[i] ^ key[i % keySize];
+    }
+    return result;
+}
+
+QByteArray cryptomanager::decrypt(const QString &data, const QString &key)
+{
+    return encrypt(data, key);
+}
+
+QString cryptomanager::encryptString(const QString &text, const QString &key)
+{
+    QByteArray data = text.toUtf8();
+    QByteArray keyData = key.toUtf8();
+    QByteArray encrypted = encrypt(data, keyData);
+    return QString::fromUtf8(encrypted.toBase64());
+}
+
+QString cryptomanager::decryptString(const QString &cipher, const QString &key)
+{
+    QByteArray data = QByteArray::fromBase64((cipher.toUtf8()));
+    QByteArray keyData = key.toUtf8();
+    QByteArray decrypted = decrypt(data, keyData);
+    return QString::fromUtf8(decrypted);
+}
